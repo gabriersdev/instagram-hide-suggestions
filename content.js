d8abc3d072a71c6cc2945760ec423a86773d0cab
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-10-19 11:37:58 -0300

    feat: adicionada remoção do sidebar de notificação

--- content.js
+++ content.js
@@ -1,12 +1,15 @@
 const hideSuggestions = () => {
   console.log("Hide Suggestions loaded! " + new Date());
-  
-  const textsToReplace = [{o: "Sugestões para você", s: "As Sugestões foram ocultadas"}, {o: "Suggestions For You", s: "Suggestions has been hide"},]
-  
+
+  const textsToReplace = [
+    { o: "Sugestões para você", s: "As Sugestões foram ocultadas" },
+    { o: "Suggestions For You", s: "Suggestions has been hide" },
+  ];
+
   const getSidebarElements = () => {
     const all = document.querySelectorAll('*');
     const matched = [];
-    
+
     all.forEach((el) => {
       const style = getComputedStyle(el);
       const expectedWidth = style.getPropertyValue('--feed-sidebar-width');
@@ -14,35 +17,37 @@ const hideSuggestions = () => {
         matched.push(el);
       }
     });
-    
+
     return matched;
   }
-  
+
   const elementsIsInSuggestionBar = (elements) => {
     // Percorre o array de elementos que for passado em busca de um elemento irmão que tenha algum texto que corresponda ao que foi definido na propriedade "o" dos elementos do array textMatch
     return elements.filter((el) => Array.from(el.parentElement.querySelectorAll("*")).find(e => textsToReplace.map(i => i.o).includes(e.textContent)));
   }
-  
+
   const hideMappedElement = () => {
-    const startsMatching = ["x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh", "x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw"];
-    
-    const endsMatching = ["x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1", "x1qjc9v5 x9f619 x78zum5 xdt5ytf xln7xf2 xk390pu x5yr21d x1n2onr6 x11njtxf xh8yej3"];
-    
-    const [selV1, selV2] = startsMatching;
-    const [selV3, selV4] = endsMatching;
-    
+    const startsMatching = ["x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh", "x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw", "html-div xdj266r x14z9mp xat24cr x1lziwak x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw xv54qhq xf7dkkf xsag5q8 xz9dl7a x1n2onr6", "html-div xdj266r x14z9mp xat24cr x1lziwak xyri2b x1c1uobl x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw xwib8y2 x1y1aw1k x1uhb9sk"];
+
+    const endsMatching = ["x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1", "x1qjc9v5 x9f619 x78zum5 xdt5ytf xln7xf2 xk390pu x5yr21d x1n2onr6 x11njtxf xh8yej3", "x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1", "x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1"];
+
+    const [selV1s, selV2s, selV3s, selV4s] = startsMatching;
+    const [selV1e, selV2e, selv3e, selv4e] = endsMatching;
+
     const allElements = document.querySelectorAll('div[class]');
-    
+    const correspondenceOk = [];
+
     const matched = Array.from(allElements).filter(el => {
       const cls = el.className.trim();
-      return cls.startsWith(selV1) || cls.startsWith(selV2) || cls.endsWith(selV3) || cls.endsWith(selV4);
+      // Se o elemento começar com a class definida em selV3s ou selV4s, já adiciona-o ao array
+      if (cls.startsWith(selV3s) || cls.startsWith(selV4s)) correspondenceOk.push(el);
+      return cls.startsWith(selV1s) || cls.startsWith(selV2s) || cls.startsWith(selV3s) || cls.startsWith(selV4s) || cls.endsWith(selV1e) || cls.endsWith(selV2e) || cls.endsWith(selv3e) || cls.endsWith(selv4e);
     });
-    
+
     let elV1 = [...matched];
     const sideBarElements = [...getSidebarElements()];
     const suggestionBarElements = [...elementsIsInSuggestionBar(matched)];
-    const correspondenceOk = [];
-    
+
     // Percorre o array de elementos elV1 que contém mais correspondências para verificar quais elementos são os mesmos do array sideBarElements e adicionar os elementos iguais ao array duoCorrespondenceElements
     elV1.forEach(e1 => {
       let correspondence;
@@ -51,41 +56,39 @@ const hideSuggestions = () => {
       else correspondence = suggestionBarElements.find(s => s === e1);
       if (correspondence) correspondenceOk.push(correspondence);
     })
-    
-    console.log("Remove: ", correspondenceOk);
-    
+
     if (correspondenceOk) {
       correspondenceOk.forEach(d => {
         // Oculta o elemento ao invés de remover do DOM
+        if (!d) return;
         d.dataset.IHS = "hidden";
         d.style.display = "none";
       });
-      
+
       // Altera o texto para informar a remoção das sugestões - se houver elemento com esse conteúdo
       Array.from(document.querySelectorAll('*')).map((el) => textsToReplace.map(i => i.o).includes(el.textContent) ? el : null).forEach(el => {
         if (el) el.textContent = textsToReplace.find(i => i.o === el.textContent)["s"];
       })
     }
   }
-  
+
   const observer = new MutationObserver(() => {
     console.log("Observe dispatched! %s", new Date());
-    
+
     // Acionando a função hideMappedElements, para varrer novamente a página atrás de correspondências
     hideMappedElement();
-    
+
     // Container da barra lateral direita
     const sidebar = document.querySelector('._ab8w');
     if (!sidebar) return;
-    
+
     const suggestionBlocks = sidebar.querySelectorAll('._aacl');
     if (!suggestionBlocks) return;
-    
+
     suggestionBlocks.forEach((block) => {
       const text = block.innerText || "";
       if (text.includes("Sugestões para você") || text.includes("Suggestions For You")) {
         const parent = block.closest('section') || block.closest('div');
-        console.log(parent ? "Parent element to remove: " + parent : "");
         if (parent) {
           // Oculta o elemento ao invés de remover do DOM
           parent.dataset.IHS = "hidden";
@@ -94,9 +97,9 @@ const hideSuggestions = () => {
       }
     });
   });
-  
-  observer.observe(document?.body, {childList: true, subtree: true});
-  
+
+  observer.observe(document?.body, { childList: true, subtree: true });
+
   setTimeout(hideMappedElement, 0);
 }
 
